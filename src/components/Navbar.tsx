a2344c1bde70cd2ea89832a2ea7305081105921a
farah-jannat <farahjannatmithila@gmail.com>
2023-12-07 22:30:03 +0600

    Import Icons in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import MaxWidthWrapper from "./MaxWidthWrapper"
+import { Icons } from "./Icons"
 
 const Navbar = () =>{
     return (
@@ -11,7 +12,9 @@ const Navbar = () =>{
                         <div className=" flex h-16 items-center">
                             {/* Todo: Modile nav  */}
                             <div className="ml-4 flex lg:ml-0">
-                                <Link href='/'></Link>
+                                <Link href='/'>
+                                    <Icons.logo className="h-10 w-10"/>
+                                </Link>
                             </div>
                         </div>
                     </div>
