106c56a85cef92c831e18b7e22d477656a883dd0
farah-jannat <farahjannatmithila@gmail.com>
2023-12-08 17:47:02 +0600

    Add style for navitems

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,25 +1,49 @@
-import { PRODUCT_CATEGORIES } from "@/config"
-import { Button } from "./ui/button"
+"use client";
 
-type Category = typeof PRODUCT_CATEGORIES[number]
+import { PRODUCT_CATEGORIES } from "@/config";
+import { Button } from "./ui/button";
+import { ChevronDown } from "lucide-react";
+import { cn } from "@/lib/utils";
 
-interface NavItemProps{
-    category: Category
-    handleOpen: () => void
-    isOpen: boolean
-    isAnyOPen: boolean
+type Category = (typeof PRODUCT_CATEGORIES)[number];
+
+interface NavItemProps {
+  category: Category;
+  handleOpen: () => void;
+  isOpen: boolean;
+  isAnyOpen: boolean;
 }
 
-const NavItem = ({} : NavItemProps) => {
-    
-    return(
-        <div className="flex">
-            <div className="relative flex items-center">
-                <Button className="gap-[1.5]"></Button>
-            </div>
+const NavItem = ({ isOpen, isAnyOpen, handleOpen, category }: NavItemProps) => {
+  return (
+    <div className="flex">
+      <div className="relative flex items-center">
+        <Button
+          className="gap-[1.5]"
+          onClick={handleOpen}
+          variant={isOpen ? "secondary" : "ghost"}
+        >
+          {" "}
+          {category.label}
+          <ChevronDown
+            className={cn("h-4 w-4 transition-all text-muted-foreground",{
+                '-rotate-180':isOpen,
+            })}
+          />
+        </Button>
+      </div>
+
+
+
+      {isOpen?(
+        <div className={cn("absolute inset-x-0 top-full text-sm text-muted-foreground",{
+            "animate-in fade-in-10 slide-in-from-top-5" :!isAnyOpen
+        })}>
+            <div className="absolute inset-0 top-1/2 bg-white shadow" aria-hidden='true'/>
         </div>
-    )
-}
+      ):null}
+    </div>
+  );
+};
 
-
-export default NavItem
+export default NavItem;
