5302825cc282324982725ebf7a6f8520f700a331
“farah-jannat” <farahjannatmithila@gmail.com>
2023-12-08 16:06:34 +0600

    Create navitem component

---
+++ src/components/NavItem.tsx
@@ -0,0 +1,25 @@
+import { PRODUCT_CATEGORIES } from "@/config"
+import { Button } from "./ui/button"
+
+type Category = typeof PRODUCT_CATEGORIES[number]
+
+interface NavItemProps{
+    category: Category
+    handleOpen: () => void
+    isOpen: boolean
+    isAnyOPen: boolean
+}
+
+const NavItem = ({} : NavItemProps) => {
+    
+    return(
+        <div className="flex">
+            <div className="relative flex items-center">
+                <Button className="gap-[1.5]"></Button>
+            </div>
+        </div>
+    )
+}
+
+
+export default NavItem
