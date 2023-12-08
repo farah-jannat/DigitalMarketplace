87e9136456718516fbbc148dd7bae732d0b1b9cc
farah-jannat <farahjannatmithila@gmail.com>
2023-12-08 22:01:51 +0600

    Add styles for right navitems in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,33 +1,80 @@
-import Link from "next/link"
-import MaxWidthWrapper from "./MaxWidthWrapper"
-import { Icons } from "./Icons"
-import NavItems from "./NavItems"
+import Link from "next/link";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { Icons } from "./Icons";
+import NavItems from "./NavItems";
+import { buttonVariants } from "./ui/button";
 
-const Navbar = () =>{
-    return (
+const Navbar = () => {
+  const user = null;
 
-        <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
-            <header className="relative bg-white">
-                <MaxWidthWrapper>
-                    <div className=" border-b border-gray-200">
-                        <div className=" flex h-16 items-center">
-                            {/* Todo: Modile nav  */}
-                            <div className="ml-4 flex lg:ml-0">
-                                <Link href='/'>
-                                    <Icons.logo className="h-10 w-10"/>
-                                </Link>
-                            </div>
+  return (
+    <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
+      <header className="relative bg-white">
+        <MaxWidthWrapper>
+          <div className=" border-b border-gray-200">
+            <div className=" flex h-16 items-center">
+              {/* Todo: Modile nav  */}
+              <div className="ml-4 flex lg:ml-0">
+                <Link href="/">
+                  <Icons.logo className="h-10 w-10" />
+                </Link>
+              </div>
 
-                            <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
-                                <NavItems />
-                            </div>
-                        </div>
+              <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
+                <NavItems />
+              </div>
+              <div className="ml-auto flex items-center">
+                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                  {user ? null : (
+                    <Link
+                      href={"/sign-in"}
+                      className={buttonVariants({ variant: "ghost" })}
+                    >
+                      {" "}
+                      Sign in{" "}
+                    </Link>
+                  )}
+
+                  {user ? null : (
+                    <span className="h-6 w-px bg-gray-200" area-hidden="true" />
+                  )}
+
+                  {user ? (
+                    <p></p>
+                  ) : (
+                    <Link
+                      href="/sign-up"
+                      className={buttonVariants({ variant: "ghost" })}
+                    >
+                      {" "}
+                      Create account
+                    </Link>
+                  )}
+                  {user ? (
+                    <span className="h-6 w-px bg-gray-200" area-hidden="true" />
+                  ) : null}
+
+                  {user ? null : (
+                    <div className="flex lg:ml-6">
+                      <span
+                        className="h-6 w-px bg-gray-200"
+                        area-hidden="true"
+                      />
                     </div>
-                </MaxWidthWrapper>
-            </header>
-        </div>
-    )
-}
+                  )}
 
 
-export default Navbar
+                  <div className="ml-4 flow-root lg:ml-6">
+                    <Cart/>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
